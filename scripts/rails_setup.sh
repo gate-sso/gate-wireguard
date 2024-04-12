90e1ec37729c1e1afd96ea0a0d669e242a054386
ajey <ajey@soracloud.ai>
2024-04-12 09:34:59 +0000

    Added Rails setup script

---
+++ scripts/rails_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y ansible wget curl
+ansible-playbook scripts/rails_setup.yml
+bundle config set --local path '.local'
+npm install npx
+npm install yarn
+bundle install
+
+
+

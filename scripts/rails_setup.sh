71a47245f37fceb7fa87d00e0446602bb6993ca4
ajey <ajey@soracloud.ai>
2024-04-14 16:10:47 +0000

    removed the architecture

--- scripts/rails_setup.sh
+++ scripts/rails_setup.sh
@@ -6,7 +6,7 @@ ansible-playbook scripts/rails_setup.yml
 sudo gem install bundler
 sudo gem install rails
 bundle config set --local path '.local'
-sudo npm install --save-exact --save-dev esbuild yarn npx
+sudo npm install --save-exact --save-dev esbuild yarn npx -g
 bundle install
 bin/rails javascript:install:esbuild
 yarn build

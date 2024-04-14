de9a7995de8b77434ec99ba965103cad000c3033
ajey <ajey@soracloud.ai>
2024-04-14 16:01:39 +0000

    updating rails setup, adding sudo where reqiured

--- scripts/rails_setup.sh
+++ scripts/rails_setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 sudo apt-get install -y ansible wget curl
 ansible-playbook scripts/rails_setup.yml
 bundle config set --local path '.local'
-npm install npx
-npm install yarn -g
-npm install --save-exact --save-dev esbuild
+sudo npm install --save-exact --save-dev esbuild yarn npx
 bundle install
+bin/rails javascript:install:esbuild
+yarn build
+esbuild app/javascript/*.* --bundle --sourcemap --outdir=app/assets/builds --public-path=assets
 #Monospace Neon, Monaco, 'Courier New', monospace
 
 

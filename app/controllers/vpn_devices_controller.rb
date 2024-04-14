9db05570a96defa59de523a03b4ddc9c39ec3c80
ajey <ajey@soracloud.ai>
2024-04-14 15:32:34 +0000

    getting to wireguard setup

--- app/controllers/vpn_devices_controller.rb
+++ app/controllers/vpn_devices_controller.rb
@@ -3,7 +3,8 @@ class VpnDevicesController < ApplicationController
 
   # GET /vpn_devices or /vpn_devices.json
   def index
-    @vpn_devices = VpnDevice.all
+    @user = current_user
+    @vpn_devices = @user.vpn_devices
   end
 
   # GET /vpn_devices/1 or /vpn_devices/1.json
@@ -12,7 +13,8 @@ class VpnDevicesController < ApplicationController
 
   # GET /vpn_devices/new
   def new
-    @vpn_device = VpnDevice.new
+    @vpn_device = current_user.vpn_devices.build
+    @config_file = WireGuardConfigGenerator.generate_config(current_user.id)
   end
 
   # GET /vpn_devices/1/edit
@@ -21,16 +23,13 @@ class VpnDevicesController < ApplicationController
 
   # POST /vpn_devices or /vpn_devices.json
   def create
-    @vpn_device = VpnDevice.new(vpn_device_params)
+    config_file = params[:config_file]
+    output, status = Open3.capture2e("sudo wg-quick up #{config_file}")
 
-    respond_to do |format|
-      if @vpn_device.save
-        format.html { redirect_to vpn_device_url(@vpn_device), notice: "Vpn device was successfully created." }
-        format.json { render :show, status: :created, location: @vpn_device }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @vpn_device.errors, status: :unprocessable_entity }
-      end
+    if status.success?
+      redirect_to vpn_devices_path, notice: 'WireGuard interface created successfully.'
+    else
+      redirect_to new_vpn_device_path, alert: "Failed to create WireGuard interface:\n#{output}"
     end
   end
 
---
+++ scripts/wireguard_setup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ansible-playbook -i inventory scripts/wireguard.yml

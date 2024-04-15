78f5a395fd019c83c083ebada4862d388f50a1f1
ajeygore <ajeygore@gmail.com>
2024-04-15 13:16:50 +0000

    Configuration page completed, need to implement delete network address

---
+++ db/migrate/20240415031445_create_vpn_configurations.rb
@@ -0,0 +1,12 @@
+class CreateVpnConfigurations < ActiveRecord::Migration[7.1]
+  def change
+    create_table :vpn_configurations do |t|
+      t.string :wg_private_key
+      t.string :wg_public_key
+      t.string :wg_ip_address
+      t.string :wg_port
+
+      t.timestamps
+    end
+  end
+end
--- db/migrate/20240415053710_rename_configuration_to_vpn_configuration.rb
+++
@@ -1,5 +0,0 @@
-class RenameConfigurationToVpnConfiguration < ActiveRecord::Migration[7.1]
-  def change
-    rename_table :configurations, :vpn_configurations
-  end
-end

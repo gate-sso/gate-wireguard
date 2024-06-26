b0fbd027fe3905269a162f87f99aa0df7141e745
Ajey Gore <ajey@soracloud.ai>
2024-06-26 06:26:48 +0000

    dot env setup, refuse to start if no environment variable setup

---
+++ config/initializers/dotenv.rb
@@ -0,0 +1,7 @@
+Dotenv.require_keys(
+  'GOOGLE_CLIENT_ID',
+  'GOOGLE_CLIENT_SECRET',
+  'GATE_REDIS_HOST',
+  'GATE_REDIS_PORT',
+  'GATE_DNS_ZONE'
+)
--- scripts/rails_setup.sh
+++ scripts/rails_setup.sh
@@ -12,9 +12,14 @@ echo 'export PATH="$PATH:~/.ruby/bin"' >> ~/.zshrc
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 20
+npm install -g yarn
 
-source ~/.bashrc
-source ~/.zshrc
+#need to source appropriate shell rc file
+if [ -n "$BASH_VERSION" ]; then
+    source ~/.bashrc
+elif [ -n "$ZSH_VERSION" ]; then
+    source ~/.zshrc
+fi
 
 ansible-playbook scripts/rails_setup.yml
 gem install bundler

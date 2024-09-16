1a0d2a11a61707c1c4fb14f1c00bfa22ea63aec9
Ajey Gore <ajey@soracloud.ai>
2024-09-16 15:53:59 +0000

    bumped rails version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_05_090543) do
+ActiveRecord::Schema[7.2].define(version: 2024_06_05_090543) do
   create_table "dns_records", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "host_name"
     t.string "ip_address"

e8cae2eec25a1b89922fc840a59fd3f8b0da6ce8
NamSupawan <60772519+NamSupawan@users.noreply.github.com>
2024-09-19 16:23:23 +0700

    auth (#1)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,33 @@
+# class SessionsControllerV2 < ApplicationController
+#   def create
+#     @user = User.from_omniauth(request.env['omniauth.auth'])
+#     session[:user_id] = @user.id
+#     redirect_to root_path
+#   end
+
+#   def destroy
+#     session[:user_id] = nil
+#     redirect_to root_path
+#   end
+# end
+
 class SessionsController < ApplicationController
   def create
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-    session[:user_id] = @user.id
-    redirect_to root_path
-  end
+    auth = request.env['omniauth.auth']
+    email_domain = auth['info']['email'].split('@').last
 
-  def destroy
-    session[:user_id] = nil
-    redirect_to root_path
+    if email_domain == ENV['HOSTED_DOMAINS']
+      # Proceed with login
+      user = User.find_or_create_by(provider: auth['provider'], uid: auth['uid']) do |u|
+        u.name = auth['info']['name']
+        u.email = auth['info']['email']
+      end
+      @user = user
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Signed in!'
+    else
+      # Reject login for other domains
+      redirect_to root_path, alert: 'Unauthorized domain.'
+    end
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,14 @@
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#     provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+#              { scope: 'profile,email', hd: ENV['HOSTED_DOMAINS']}
+# end
+
+# OmniAuth.config.allowed_request_methods = %i[get]
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
-             { scope: 'profile,email', hd: ENV['HOSTED_DOMAINS'], redirect_uri: 'https://production-vpn-wireguard.flowaccount.com/auth/google_oauth2/callback'}
+      { scope: 'userinfo.profile,userinfo.email',prompt: 'select_account',email_verified: true, hd: ENV['HOSTED_DOMAINS'], redirect_uri:  ENV['GOOGLE_REDIRECT_URI']}
 
 end
 
 OmniAuth.config.allowed_request_methods = %i[get]
+~                                                        

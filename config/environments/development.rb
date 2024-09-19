915e8dd4a5560b9129334154b613120afb8af47c
NamSupawan <60772519+NamSupawan@users.noreply.github.com>
2024-09-19 16:23:23 +0700

    auth (#1)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,10 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
-  config.hosts = ["172.10.4.224",IPAddr.new("172.10.0.0/24"),"production-vpn-wireguard.flowaccount.com"]
-  
-  
+  config.hosts = [ENV["IPCONFIG"],IPAddr.new(ENV["IPCONFIG_RANG"]),ENV["DOMAINS_VPN"]]
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.

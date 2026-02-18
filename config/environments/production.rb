cdde9d553b7a0f67774549376984fb4140e21f42
Ajey Gore <ajeygore@gmail.com>
2026-02-19 00:31:38 +0800

    Updated to rails 8.1.2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,12 +46,8 @@ Rails.application.configure do
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
-  # Assume all access to the app is happening through a SSL-terminating reverse proxy.
-  # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
-  # config.assume_ssl = true
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # SSL is terminated by Caddy. Puma serves plain HTTP on localhost.
+  # config.force_ssl = false
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new($stdout)

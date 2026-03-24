1c3b68461ad4ee70dee6eb7462f8fe3e720fb962
Lakshay <lakshaybadak@gmail.com>
2026-03-24 13:28:47 +0530

    removing hosted domain from omniauth initializer as we already have user validation upon signup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV.fetch('GOOGLE_CLIENT_ID', nil), ENV.fetch('GOOGLE_CLIENT_SECRET', nil),
-           { scope: 'profile,email', hd: ENV.fetch('HOSTED_DOMAINS', nil) }
+           { scope: 'profile,email' }
 end
 
 OmniAuth.config.allowed_request_methods = %i[get]
